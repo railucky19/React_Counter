a56ee053a6a474d3066203c448bed6759e219b59
Lucky Rai <lucky.rai@waveaxis.co.in>
2021-05-03 02:50:15 +0530

    first commit

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 5
+        }
+    }
+    increment = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+    decrement = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    neutral = () => {
+        this.setState({ number: 0 })
+    }
+    render() {
+        return (
+            <div className="row justify-content-center">
+                <div className="col-md-5 " style={{ marginTop: "150px", border: "5px solid black" }}>
+                    <h1>React Counter</h1>
+                    <h1>{this.state.number}</h1>
+                    <button className="btn btn-success" onClick={this.increment}>INCREMENT</button>
+                    <button className="btn btn-danger" onClick={this.decrement}>DECREMENT</button>
+                    <button className="btn btn-primary" onClick={this.neutral}>NEUTRAL</button>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Main;
